109324dcdeb2698daf602c4f092b2c523d9610c3
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2024-09-18 15:03:00 +0300

    feat(Swagger): add swagger documentation on /api

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,11 @@ async function bootstrap() {
       },
     }),
   );
+
+  const swagConfig = new DocumentBuilder().build();
+
+  const document = SwaggerModule.createDocument(app, swagConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(4000);
 }
